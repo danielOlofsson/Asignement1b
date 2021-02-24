8530dc39b37d1fa4b5cec738e46ed9fd29bb50db
Daniel <daniel.e.olofsson@telia.com>
2021-02-24 13:35:45 +0100

    fixade errors körbar.

--- servermain.cpp
+++ servermain.cpp
@@ -37,59 +37,149 @@ void checkJobbList(int signum){
   return;
 }
 
-void convertCalcMsgToPrintable(calcMessage &clientMsg)
+void convertCalcMsgToPrintable(struct calcMessage* clientMsg)
 {
-  clientMsg.type = ntohs(clientMsg.type);
-  clientMsg.message = nthol(clientMsg.message);
-  clientMsg.protocol = ntohs(clientMsg.protocol);
-  clientMsg.major_version = ntohs(clientMsg.major_version);
-  clientMsg.major_version = ntohs(clientMsg.minor_version);
+  clientMsg->type = ntohs(clientMsg->type);
+  clientMsg->message = ntohl(clientMsg->message);
+  clientMsg->protocol = ntohs(clientMsg->protocol);
+  clientMsg->major_version = ntohs(clientMsg->major_version);
+  clientMsg->major_version = ntohs(clientMsg->minor_version);
 }
 
-void convertCalcMsgToSendable(calcMessage &serverMsg)
+void convertCalcMsgToSendable(struct calcMessage* serverMsg)
 {
-  clientMsg.type = htons(clientMsg.type);
-  clientMsg.message = htonl(clientMsg.message);
-  clientMsg.protocol = htons(clientMsg.protocol);
-  clientMsg.major_version = htons(clientMsg.major_version);
-  clientMsg.major_version = htons(clientMsg.minor_version);
+  serverMsg->type = htons(serverMsg->type);
+  serverMsg->message = htonl(serverMsg->message);
+  serverMsg->protocol = htons(serverMsg->protocol);
+  serverMsg->major_version = htons(serverMsg->major_version);
+  serverMsg->major_version = htons(serverMsg->minor_version);
 }
 
-void convertCalcProtocolToPrintable(calcProtocol &clientMsg)
+void convertCalcProtocolToPrintable(struct calcProtocol* clientMsg)
 {
-  clientMsg.type = ntohs(clientMsg->type);
-  clientMsg.major_version = ntohs(clientMsg->major_version);
-  clientMsg.minor_version = ntohs(clientMsg->minor_version);
-  clientMsg.id = ntohl(clientMsg->id);
-  clientMsg.arith = ntohl(clientMsg->arith);
-  clientMsg.inValue1 = ntohl(clientMsg->inValue1);
-  clientMsg.inValue2 = ntohl(clientMsg->inValue2);
-  clientMsg.inResult = ntohl(clientMsg->inResult);
+  clientMsg->type = ntohs(clientMsg->type);
+  clientMsg->major_version = ntohs(clientMsg->major_version);
+  clientMsg->minor_version = ntohs(clientMsg->minor_version);
+  clientMsg->id = ntohl(clientMsg->id);
+  clientMsg->arith = ntohl(clientMsg->arith);
+  clientMsg->inValue1 = ntohl(clientMsg->inValue1);
+  clientMsg->inValue2 = ntohl(clientMsg->inValue2);
+  clientMsg->inResult = ntohl(clientMsg->inResult);
 }
 
-void convertCalcProtocolToPrintable(calcProtocol &serverMsg)
+void convertCalcProtocolToSendable(calcProtocol *serverMsg)
 {
-  clientMsg.type = htons(clientMsg->type);
-  clientMsg.major_version = htons(clientMsg->major_version);
-  clientMsg.minor_version = htons(clientMsg->minor_version);
-  clientMsg.id = htonl(clientMsg->id);
-  clientMsg.arith = htonl(clientMsg->arith);
-  clientMsg.inValue1 = htonl(clientMsg->inValue1);
-  clientMsg.inValue2 = htonl(clientMsg->inValue2);
-  clientMsg.inResult = htonl(clientMsg->inResult);
+  serverMsg->type = htons(serverMsg->type);
+  serverMsg->major_version = htons(serverMsg->major_version);
+  serverMsg->minor_version = htons(serverMsg->minor_version);
+  serverMsg->id = htonl(serverMsg->id);
+  serverMsg->arith = htonl(serverMsg->arith);
+  serverMsg->inValue1 = htonl(serverMsg->inValue1);
+  serverMsg->inValue2 = htonl(serverMsg->inValue2);
+  serverMsg->inResult = htonl(serverMsg->inResult);
 }
 
 
-bool checkIfSupports(calcMessage clientMsg)
+bool checkIfSupports(calcMessage* clientMsg)
 {
   bool supports = false;
-  if(clientMsg.major_version == 1 && clientMsg.message == 0 && clientMsg.minor_version = 0 && clientMsg.protocol = 17 && clientMsg.type == 22)
+  if(clientMsg->major_version == 1 && clientMsg->message == 0 && clientMsg->minor_version == 0 && clientMsg->protocol == 17 && clientMsg->type == 22)
   {
-    supports == true;
+    supports = true;
   }
   return supports;
 }
   
+
+void initiateRandomCalcProtocol(calcProtocol *protocolMsg)
+{
+  double fv1,fv2,fresult;
+  int iv1 ,iv2 ,iresult;
+  char msg[256];
+  char *op = randomType();
+
+  protocolMsg->type = 1;
+  protocolMsg->major_version = 1;
+  protocolMsg->minor_version = 0;
+  
+  memset(msg, 0, sizeof(msg));
+
+  if(op[0] == 'f')
+  {
+    #ifdef DEBUG
+    printf("Float\t");
+    #endif
+    fv1=randomFloat();
+    fv2=randomFloat();
+
+
+    protocolMsg->flValue1 = fv1;
+    protocolMsg->flValue2 = fv2;
+    protocolMsg->flResult = (double)0;
+    protocolMsg->inValue1 = 0;
+    protocolMsg->inValue2 = 0;
+    protocolMsg->inResult = 0;
+
+    if(strcmp(op,"fadd")==0)
+    {
+      //fresult=fv1+fv2;
+      protocolMsg->arith = 5;
+    } 
+    else if (strcmp(op, "fsub")==0)
+    {
+      //fresult=fv1-fv2;
+      protocolMsg->arith = 6;
+    } 
+    else if (strcmp(op, "fmul")==0)
+    {
+      //fresult=fv1*fv2;
+      protocolMsg->arith = 7;
+    } 
+    else if (strcmp(op, "fdiv")==0)
+    {
+      //fresult=fv1/fv2;
+      protocolMsg->arith = 8;
+    }
+    #ifdef DEBUG
+    printf("Servers result: %8.8g",fresult);
+    #endif
+  }
+  else
+  {
+    iv1 = randomInt();
+    iv2 = randomInt();
+    protocolMsg->inValue1 = iv1;
+    protocolMsg->inValue2 = iv2;
+    protocolMsg->inResult = 0;
+    protocolMsg->flValue1 = (double)0;
+    protocolMsg->flValue2 = (double)0;
+    protocolMsg->flResult = (double)0;
+    if(strcmp(op,"add")==0)
+    {
+      //iresult=iv1+iv2;
+      protocolMsg->arith = 1;
+    } 
+    else if (strcmp(op, "sub")==0)
+    {
+      //iresult=iv1-iv2;
+      protocolMsg->arith = 2;
+    }
+    else if (strcmp(op, "mul")==0)
+    {
+      //iresult=iv1*iv2;
+      protocolMsg->arith = 3;
+    } 
+    else if (strcmp(op, "div")==0)
+    {
+      //iresult=iv1/iv2;
+      protocolMsg->arith = 4;
+    }
+    #ifdef DEBUG
+    printf("Servers result: %d",iresult);
+    #endif
+  }
+  convertCalcProtocolToSendable(protocolMsg);
+}
 
 
 int main(int argc, char *argv[]){
@@ -140,7 +230,7 @@ int main(int argc, char *argv[]){
   int recivedValue;
   int serverSocket;
   int numbytes;
-
+  int yes = 1;
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC; // AF_INET , AF_INET6
   hints.ai_socktype = SOCK_DGRAM; // <<--- TRANSPORT PROTOCOL!!
@@ -222,24 +312,25 @@ int main(int argc, char *argv[]){
 		}
     else
     {
-      savedClients[0]->ai_addr = p->ai_addr;
-      savedClients[0]->ai_addrlen = p->ai_addrlen;
+      savedClients[0].ai_addr = p->ai_addr;
+      savedClients[0].ai_addrlen = p->ai_addrlen;
       break;
     }
 
     //Client Skickar sitt svar.... 
     if(numbytes == sizeof(calcProtocol))
     {
-
+      
     }
     else
     {
       msgPtr = (calcMessage *)calcPtr;
-
-
+      convertCalcMsgToPrintable(msgPtr);
+      if(checkIfSupports(msgPtr) == true)
+      {
+        //protocol supported initiate and send calcprotocol
+      }
     }
-
-
 
 
   }
